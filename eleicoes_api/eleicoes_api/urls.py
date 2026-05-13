2b504baceb8a58bffb596ac4af31801583e2b471
Pedro <rpedroricardo87@gmail.com>
2026-05-13 14:57:08 -0300

    third commit - questão 3

--- eleicoes_api/eleicoes_api/urls.py
+++ eleicoes_api/eleicoes_api/urls.py
@@ -15,9 +15,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from rest_framework.routers import DefaultRouter
+from urna import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Eleições API",
+      default_version='v1',
+      description="API REST para gerenciamento de eleições, candidatos e votos.",
+   ),
+   public=True,
+)
+
+router = DefaultRouter()
+router.register(r'eleitores', views.EleitorViewSet)
+router.register(r'eleicoes', views.EleicaoViewSet)
+router.register(r'candidatos', views.CandidatoViewSet)
+router.register(r'aptidoes', views.AptidaoEleitorViewSet)
+router.register(r'registros-votacao', views.RegistroVotacaoViewSet)
+router.register(r'votos', views.VotoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('eleicoes_api/', include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- eleicoes_api/urna/views.py
+++ eleicoes_api/urna/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.exceptions import MethodNotAllowed
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class EleitorViewSet(viewsets.ModelViewSet):
+    queryset = Eleitor.objects.all()
+    serializer_class = EleitorSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['ativo']
+    search_fields = ['nome', 'email', 'cpf']
+
+
+class EleicaoViewSet(viewsets.ModelViewSet):
+    queryset = Eleicao.objects.all()
+    serializer_class = EleicaoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status', 'tipo', 'criada_por']
+    search_fields = ['titulo']
+    ordering_fields = ['data_inicio']
+    ordering = ['data_inicio']
+
+
+class CandidatoViewSet(viewsets.ModelViewSet):
+    queryset = Candidato.objects.select_related('eleicao').all()
+    serializer_class = CandidatoSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['eleicao']
+    search_fields = ['nome', 'nome_urna', 'partido_ou_chapa']
+
+
+class AptidaoEleitorViewSet(viewsets.ModelViewSet):
+    queryset = AptidaoEleitor.objects.select_related('eleitor', 'eleicao').all()
+    serializer_class = AptidaoEleitorSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['eleitor', 'eleicao']
+
+
+class RegistroVotacaoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = RegistroVotacao.objects.select_related('eleitor', 'eleicao').all()
+    serializer_class = RegistroVotacaoSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['eleicao']
+    ordering_fields = ['data_hora']
+    ordering = ['-data_hora']
+
+
+class VotoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Voto.objects.select_related('candidato', 'eleicao').all()
+    serializer_class = VotoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['eleicao']
+    http_method_names = ['get', 'head', 'options']
+
+    def create(self, request, *args, **kwargs):
+        raise MethodNotAllowed('POST')
+
+    def update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PUT')
+
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed('PATCH')
+
+    def destroy(self, request, *args, **kwargs):
+        raise MethodNotAllowed('DELETE')
